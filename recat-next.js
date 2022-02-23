44d41a4e6b242aab15c1edf81da752816efcdf2b
namit <namit.mcodeinfosof@gmail.com>
2022-02-23 16:03:30 +0530

    react next js

---
+++ recat-next.js
@@ -0,0 +1,22 @@
+const valid=()=>{
+    if(typeof window !== "undefined" && window.glob){
+        return true
+    }
+    return false
+  }
+  const logEvent=(name,pr={})=>{
+    if(valid()){
+        _1flow.logEvent(name,pr);
+    }else{
+        setTimeout(()=>{logEvent(name,pr)},10);
+    }
+  }
+  const logUser=(system_id,pr={})=>{
+  if(valid()){
+    _1flow.logUser(system_id,pr);
+  }else{
+    setTimeout(()=>{logUser(system_id,pr)},10);
+  }
+  }
+  module.exports= {logEvent,logUser}
+  
